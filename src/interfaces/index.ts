aea3f5d626d4336473edc292dae44f987428081f
Douglas Ramires <douglasramires01@gmail.com>
2025-03-27 16:05:25 -0300

    feat: Estruturando tipos de retorno para User

---
+++ src/interfaces/index.ts
@@ -0,0 +1,20 @@
+export interface IUserRequest {
+    name: string
+    email: string
+    password: string
+    age: number
+}
+
+export interface IUResponseCreate {
+    name: string
+    email: string
+    age: number
+}
+
+export interface IUserresponse {
+    name: string
+    email: string
+    password: string
+    age: number
+    id: string
+}
