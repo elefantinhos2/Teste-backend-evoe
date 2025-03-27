4120e4852fe615bbff511d1a49b7355a56225a64
Douglas Ramires <douglasramires01@gmail.com>
2025-03-27 16:01:48 -0300

    feat: Criando Rotas e adicionado Path

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
 import express from 'express';
+import userRoutes from './routes';
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/users', userRoutes);
 
 export default app;
