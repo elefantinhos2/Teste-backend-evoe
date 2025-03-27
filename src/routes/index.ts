13d5ee5792456d1506d8c4b4cbc196ede8d694dd
Douglas Ramires <douglasramires01@gmail.com>
2025-03-27 16:22:46 -0300

    feat: Desenvolvendo Rotas para delete e retorno de dados unicos de user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,14 @@
 import Router from 'express';
-import { createUserController, listUserControllers } from '../controllers/user.controller';
+import { createUserController, listUserControllers, retrieveUserController, deleteUserController } from '../controllers/user.controller';
 
 const userRoutes = Router();
 
 userRoutes.post('', createUserController);
 
 userRoutes.get('', listUserControllers);
+
+userRoutes.get('/:userId', retrieveUserController);
+
+userRoutes.delete('/:userId', deleteUserController);
 
 export default userRoutes;
